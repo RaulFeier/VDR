66365e53aac1ac5adb505425d8afedc109616729
RaulFeier <ionelafeier1@gmail.com>
2023-02-14 12:48:10 +0200

    eep

---
+++ main.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+img = Image.open("eep.png")
+img.show()
---
+++ melodie.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  cout << "Name: The Last of Us Theme | 1 Hour Classical Guitar Cover" << endl;
+  cout << "Link: https://www.youtube.com/watch?v=8YQDeWW6euE" << endl;
+}
